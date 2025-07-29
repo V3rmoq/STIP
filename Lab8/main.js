1ed1ca272b155181bcacaf196150e40f93d75ebb
Bafence <135432384+V3rmoq@users.noreply.github.com>
2025-07-29 23:18:52 +0300

    Update main.js

--- Lab8/main.js
+++ Lab8/main.js
@@ -1,5 +1,38 @@
-$(document).ready(function() {
-    let xmlfile = fetch("cookbook.xml").then(res => res.text().then(txt => $("#xml").text(txt)));
-    let dtdfile = fetch("cookbook.dtd").then(res => res.text().then(txt => $("#dtd").text(txt)));
-    let xsdfile = fetch("cookbook.xsd").then(res => res.text().then(txt => $("#xsd").text(txt)));
+document.addEventListener('DOMContentLoaded', function() {
+    // Загрузка XML
+    fetch('cookbook.xml')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('xml-block').innerHTML = 
+                `<pre>${escapeHtml(data)}</pre>`;
+        })
+        .catch(error => console.error('Error loading XML:', error));
+
+    // Загрузка DTD
+    fetch('cookbook.dtd')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('dtd-block').innerHTML = 
+                `<pre>${escapeHtml(data)}</pre>`;
+        })
+        .catch(error => console.error('Error loading DTD:', error));
+
+    // Загрузка XSD
+    fetch('cookbook.xsd')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('xsd-block').innerHTML = 
+                `<pre>${escapeHtml(data)}</pre>`;
+        })
+        .catch(error => console.error('Error loading XSD:', error));
+
+    // Функция для экранирования HTML-символов
+    function escapeHtml(unsafe) {
+        return unsafe
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
 });
