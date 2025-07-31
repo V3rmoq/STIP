eb69750898c260816526577dcae2ecacc3f2a194
Bafence <135432384+V3rmoq@users.noreply.github.com>
2025-07-31 04:39:16 +0300

    Update main.js

--- Lab8/main.js
+++ Lab8/main.js
@@ -1,5 +1,24 @@
-$(document).ready(function() {
-    let xmlfile = fetch("cookbook.xml").then(res => res.text().then(txt => $("#xml").text(txt)));
-    let dtdfile = fetch("cookbook.dtd").then(res => res.text().then(txt => $("#dtd").text(txt)));
-    let xsdfile = fetch("cookbook.xsd").then(res => res.text().then(txt => $("#xsd").text(txt)));
-});
+// Функция для загрузки и отображения содержимого файла
+function loadFileContent(filename, elementId) {
+    fetch(filename)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Ошибка загрузки файла: ${filename}`);
+            }
+            return response.text();
+        })
+        .then(content => {
+            document.getElementById(elementId).textContent = content;
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById(elementId).textContent = error.message;
+        });
+}
+
+// Загружаем содержимое всех файлов при открытии страницы
+window.onload = function() {
+    loadFileContent('schema.dtd', 'dtd-content');
+    loadFileContent('data.xml', 'xml-content');
+    loadFileContent('schema.xsd', 'xsd-content');
+};
