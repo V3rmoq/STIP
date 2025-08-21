7d5001d90a76a7c5f33520eb6f680a7bc2131be0
Bafence <135432384+V3rmoq@users.noreply.github.com>
2025-08-21 19:29:45 +0300

    Update main.js

--- Lab7/main.js
+++ Lab7/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-    fetch("subscriptions.xml").then(res => res.text().then(txt => $("#xml").text(txt)));
-});
+// Функція для завантаження та відображення XML вмісту
+function loadAndDisplayXML() {
+    // Створюємо XMLHttpRequest для завантаження файлу
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'cookbook.xml', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Відображаємо XML у вікні браузера
+            document.getElementById('xmlDisplay').textContent = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+// Завантажуємо XML при завантаженні сторінки
+window.onload = loadAndDisplayXML;
